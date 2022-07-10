2ecb8b8168eb717e07d76418b7ef167b988a26ea
elad361 <72965673+elad361@users.noreply.github.com>
2022-07-10 11:21:30 +0300

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -8,23 +8,23 @@ using namespace std;
 int main(){
     StackHandler handler;
     handler.command("PUSH hey");
-    assert(handler.command("TOP") == "hey");
+    assert(handler.command("TOP") == " OUTPUT: hey");
     handler.command("PUSH hello");
     handler.command("POP");
-    assert(handler.command("TOP") == "hey");
+    assert(handler.command("TOP") == "OUTPUT: hey");
     handler.command("POP");
-    assert(handler.command("TOP") == "STACK IS EMPTY");
+    assert(handler.command("TOP") == "ERROR: Stack is empty");
     handler.command("POP");
-    assert(handler.command("TOP") == "STACK IS EMPTY");
+    assert(handler.command("TOP") == "ERROR: Stack is empty");
     char c = 97;
     for (size_t i = 0; i < 20; i++)
     {
         string check;
         check.push_back(c);
-        handler.command("PUSH check");
+        handler.command("PUSH " + check);
         c++;
     }
-    assert(handler.command("TOP") == "t");
+    assert(handler.command("TOP") == " OUTPUT: t");
 
     cout << "Test has been done successfully" << endl;
 }
